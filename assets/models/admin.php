0f93c00a8b5c924e5666b5de4287c638cda2a421
Ingrid Rodrigues <ingridcruz@infojr.com.br>
2017-06-04 16:42:28 -0300

    Adiciona imagem do gráfico

--- assets/models/admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('../heart/model/base.php');
-
-    class Admin extends \Model\Base {
-        public $fillable = ['matricula', 'senha', 'nome'];
-    }
-?>
--- assets/models/daily.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once('../heart/model/base.php');
-
-    class Daily extends \Model\Base {
-        public $fillable = ['data', 'descricao'];
-    }
-?>
